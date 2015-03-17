217ad2bb33553006f87a97df08cb3da578bb5f05
Gabriel Kroll <gabriel.kroll@me.com>
2015-03-17 22:24:48 +0100

    commit changes

---
+++ Drag And Done/DNDEditTaskHandler.swift
@@ -0,0 +1,11 @@
+//
+//  DNDEditTaskHandler.swift
+//  Drag And Done
+//
+//  Created by Gabriel Kroll on 17/03/15.
+//  Copyright (c) 2015 FreakoSoft. All rights reserved.
+//
+
+import Foundation
+
+
